18acabe1bad5043047428ae00ee1f105534e86e9
Cibele Watt <cibelewatt@gmail.com>
2021-11-12 22:31:25 -0300

    Products RestAPI Functionalities

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -12,7 +12,8 @@ namespace LetsShop.Controllers
     [ApiController]
     public class ProductsController : ControllerBase
     {
-        private List<Product> Products = new List<Product>();
+        private static List<Product> Products = new List<Product>();
+        private static int CurrentId = 0;
 
         [HttpPost]
         [Route("create")]
@@ -20,17 +21,56 @@ namespace LetsShop.Controllers
         {
             var existingProduct = Products.Where(x => x.Name == product.Name);
 
-            if (existingProduct.Any()) return StatusCode(409, "Um produto com esse nome já existe em nossa loja."); //208/422
-            else return StatusCode(201, "O produto foi adicionado com sucesso!");
+            if (existingProduct.Any())
+            {
+                return StatusCode(409, "Um produto com esse nome já existe em nossa loja."); //208/422
+            }
+            else
+            {
+                product.Id = CurrentId++;
+                Products.Add(product);
+                return StatusCode(201, "O produto foi adicionado com sucesso!");
+            }
         }
 
         [HttpGet]
-        [Route("products")]
+        [Route("all")]
         public IActionResult GetProducts()
         {
             return Ok(Products);
         }
 
+        [HttpPatch]
+        [Route("update/{index}")]
+        public IActionResult UpdateProduct([FromRoute] int index, [FromBody] Product product)
+        {
+            try
+            {
+                product.Id = Products[index].Id;
+                Products[index] = product;
+                return Ok(Products[index]);
+            }
+            catch
+            {
+                return StatusCode(500, "Erro.");
+            }
+        }
+
+        [HttpDelete]
+        [Route("delete/{index}")]
+        public IActionResult DeleteProduct([FromRoute] int index)
+        {
+            try
+            {
+                Product productToRemove = Products[index];
+                Products.RemoveAt(index);
+                return Ok("Produto Removido: " + productToRemove.Name);
+            }
+            catch
+            {
+                return StatusCode(500, "Erro.");
+            }
+        }
 
     }
 }

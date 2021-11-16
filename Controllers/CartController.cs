ec49cfe4e92ed77c2914e9ba1ee45baff89c30a2
Cibele Watt <cibelewatt@gmail.com>
2021-11-15 22:19:24 -0300

    cartcontroller modifications

--- Controllers/CartController.cs
+++ Controllers/CartController.cs
@@ -12,7 +12,8 @@ namespace LetsShop.Controllers
     [ApiController]
     public class CartController : ControllerBase
     {
-        private static List<Product> ProductsInCart = new List<Product>();
+        private static List<ProductInCart> ProductsInCart = new List<ProductInCart>();
+        private static int CurrentId = 0;
 
         [HttpGet]
         public IActionResult GetProductsInCart()
@@ -22,17 +23,47 @@ namespace LetsShop.Controllers
 
         [HttpPost]
         [Route("Products/add/{productIndex}")]
-        public IActionResult AddProduct([FromRoute] int productIndex)
+        public IActionResult AddProduct([FromBody] ProductInCart productInCart)
         {
-            try
+
+            //verifica se existe no carrinho
+            var existingProductInCart = ProductsInCart.Where(x => x.Id == productInCart.Id);
+
+            //verifica se o produto realmente existe
+            var existingProduct = ProductsController.Products.Where(x => x.Id == productInCart.Id);
+
+            if (existingProduct.Any())
             {
-                ProductsInCart.Add(ProductsController.Products[productIndex]);
-                return Ok("O produto " + ProductsController.Products[productIndex].Name + " foi adicionado ao carrinho com sucesso!");
-            }
-            catch
+                if (existingProductInCart.Any())
+                {
+                    try
+                    {
+                        existingProductInCart.FirstOrDefault().Quantity += productInCart.Quantity;
+                        return Ok("Quantidade do produto " + productInCart.Id + " atualizada com sucesso. ");
+                    }
+                    catch
+                    {
+                        return StatusCode(404);
+                    }
+                }
+                else
+                {
+                    try
+                    {
+                        ProductsInCart.Add(productInCart);
+                        return Ok("Produto " + productInCart.Id + " adicionado ao carrinho com sucesso. ");
+                    }
+                    catch
+                    {
+                        return StatusCode(404);
+                    }
+                    
+                }
+            } else
             {
-                return StatusCode(501, "O produto de index " + productIndex + " não existe em nossa loja.");
+                return StatusCode(501, "O produto de index " + productInCart.Id + " não existe em nossa loja.");
             }
+
         }
 
         [HttpDelete]
---
+++ Models/ProductInCart.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LetsShop.Models
+{
+    public class ProductInCart
+    {
+
+        public int Id { get; set; }
+
+        public int ProductId { get; set; }
+
+        public int Quantity { get; set; }
+
+    }
+}
